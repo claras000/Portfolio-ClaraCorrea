cd94961fa9923f08faf575cc780b63db91c85be2
claras000 <clara.o.correa@web.de>
2024-03-05 09:55:37 +0000

    verbingungen zu projekten

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -205,7 +205,7 @@ const Home = () => {
  ${index % 2 !== 0 ? "ml-auto md:ml-20" : " ml-auto"}`}
               >
                 <a
-                  href={`/project/${project.id}`}
+                  href={`/Portfolio-ClaraCorrea/project/${project.id}`}
                   component="0"
                   className="cursor-pointer"
                 >
